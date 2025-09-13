e132d73b15957f767d1ab41101e4c713df774bad
Vinicius Carvalho <vinicarvalho.novais@gmail.com>
2025-09-13 10:31:54 -0300

    Adicionada API criado com flex

---
+++ bd_livros.py
@@ -0,0 +1,12 @@
+livros = [
+    {'id': 1, 'título': "Guerra dos Mundos", 'autor': 'H.G. Wells', 'ano': 2001},
+    {'id': 2, 'título': "Pequeno Príncipe", 'autor': 'Antoine de Saint-Exupéry', 'ano': 1943},
+    {'id': 3, 'título': "Harry Potter", 'autor': 'J.K. Rowling', 'ano': 1997},
+    {'id': 4, 'título': "Manipulação do FBI", 'autor': 'Autor Desconhecido', 'ano': 2001},
+    {'id': 5, 'título': "Invencível", 'autor': 'Autor Desconhecido', 'ano': 2001}
+]
+
+
+
+
+
